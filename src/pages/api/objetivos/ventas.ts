2ff562344f17d2ea6834a7437c0aca800851790b
maos1111ch <125289529+maos1111ch@users.noreply.github.com>
2023-11-15 12:26:04 -0300

    add obtetivos ventas

---
+++ src/pages/api/objetivos/ventas.ts
@@ -0,0 +1,64 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import {
+  getFacturacionFechas,
+  getFacturacionTotal,
+  getFacturacionQ,
+} from "@/utils/queryDatabase";
+
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+  const { method } = request;
+
+  switch (method) {
+    case "GET":
+      const [dataFacturacionTotal] = (await getFacturacionTotal()).rows;
+      const dataFacturacionMeses = (await getFacturacionFechas()).rows;
+      const dataFacturacionQ = (await getFacturacionQ()).rows;
+
+      //Facturacion mensual
+      const ventasObj: { [key: string]: number } = {};
+      dataFacturacionMeses.forEach((item) => {
+        const mesNumero = parseInt(item.mes.split("-")[1], 10);
+        ventasObj[`ventas_${mesNumero.toString().padStart(2, "0")}`] =
+          parseFloat(item.valor_total_ventas);
+      });
+
+      //Facturacion Q
+      const ventasQObj = {
+        ventas_q1: parseFloat(
+          dataFacturacionQ.find((item) => item.cuartil === 1)
+            ?.valor_total_cuartil || "0"
+        ),
+        ventas_q2: parseFloat(
+          dataFacturacionQ.find((item) => item.cuartil === 2)
+            ?.valor_total_cuartil || "0"
+        ),
+        ventas_q3: parseFloat(
+          dataFacturacionQ.find((item) => item.cuartil === 3)
+            ?.valor_total_cuartil || "0"
+        ),
+        ventas_q4: parseFloat(
+          dataFacturacionQ.find((item) => item.cuartil === 4)
+            ?.valor_total_cuartil || "0"
+        ),
+      };
+
+      const dataResponse = [
+        {
+          ventas_anuales: parseFloat(dataFacturacionTotal.valor_total_ventas),
+          ...ventasObj,
+          ...ventasQObj,
+        },
+      ];
+
+      response
+        .status(200)
+        .json({ success: true, message: "OK", data: dataResponse });
+      break;
+
+    default:
+      response
+        .status(400)
+        .json({ success: false, message: "method invalid.", data: null });
+      break;
+  }
+};
