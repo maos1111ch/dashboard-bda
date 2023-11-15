c46c5e6b0b1117482ce43abf171b8b798d630438
Franco <francopignatelli15@gmail.com>
2023-11-15 02:42:03 -0300

    Agregada consulta a la API para shortcuts. Modificado dataComponent e invocado dentro del componente padre index.tsx

--- src/components/appData/dataComponent.tsx
+++ src/components/appData/dataComponent.tsx
@@ -10,59 +10,11 @@ const getRandomColor = () => {
   return color;
 };
 
-const DataComponent: React.FC = () => {
-  // Estado local para almacenar la respuesta simulada de la API
-  const [apiData, setApiData] = useState<any[]>([]);
-
-  // Efecto secundario para simular la llamada a la API al montar el componente
-  useEffect(() => {
-    // Función para simular la llamada a la API y actualizar el estado
-    const fetchData = async () => {
-      // Simulación de la respuesta de la API
-      const mockApiResponse = {
-        success: true,
-        message: 'OK',
-        data: [
-          {
-            title: 'title1',
-            value: 'asdasdas',
-          },
-          {
-            title: 'title2',
-            value: 'asdasdas',
-          },
-          {
-            title: 'title3',
-            value: 'asdasdas',
-          },
-          {
-            title: 'title1',
-            value: 'asdasdas',
-          },
-          {
-            title: 'title2',
-            value: 'asdasdas',
-          },
-          {
-            title: 'title3',
-            value: 'asdasdas',
-          },
-        ],
-      };
-
-      // Actualiza el estado con los datos simulados
-      setApiData(mockApiResponse.data);
-    };
-
-    // Llama a la función para simular la llamada a la API
-    fetchData();
-  }, []); // El segundo parámetro del useEffect asegura que se ejecute solo al montar el componente.
-
-  // Renderiza el componente DataComponent
+const DataComponent: React.FC<{shortcuts:any[]}> = ({shortcuts}:{shortcuts:any[]}) => {
   return (
     <div className="grid grid-cols-3 gap-4 p-4">
       <h1 className="col-span-3 text-2xl mb-4">Mi Dashboard</h1>
-      {apiData.map((item, index) => (
+      {shortcuts.map((item, index) => (
         <div
           key={index}
           className={`rounded-md p-4 mb-4 text-white`}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,25 @@
+import DataComponent from "@/components/appData/dataComponent";
 import VentasTable from "@/components/ventas/table";
 import { generarProductos } from "@/helpers/mock_data/productos";
 import { generarResumenPedidosPorProducto } from "@/helpers/mock_data/resumen_pedidos_por_producto";
 import { ResumenPedidosPorProducto } from "@/types/negocio";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next/types";
 import { useState, useEffect } from "react";
+import shortcuts from "./api/shortcuts";
 
 
 export const getServerSideProps = (async (context) => {
   const res = await fetch(`${process.env.API_URL}/api/productos`)
   // const { data: productos }: {data: ResumenPedidosPorProducto[]} = await res.json()
+  const {data:resShortcuts}:any = await (await fetch(`${process.env.API_URL}/api/shortcuts`)).json()
   const productos = null;
-  return { props: { productos } }
+  return { props: { productos, shortcuts:resShortcuts} }
 }) satisfies GetServerSideProps<{
   productos: ResumenPedidosPorProducto[] | null
+  shortcuts: any
 }>
 
-export default function Index({ }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Index({shortcuts}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const [productos, setProductos] = useState<ResumenPedidosPorProducto[]>();
 
   useEffect(() => {
@@ -26,8 +30,10 @@ export default function Index({ }: InferGetServerSidePropsType<typeof getServerS
   if (!productos) {
     return <>Loading...</>;
   }
+  console.log(shortcuts)
   return (
     <>
+      <DataComponent shortcuts={shortcuts}/>
       <VentasTable productos={productos} />
     </>
   );
